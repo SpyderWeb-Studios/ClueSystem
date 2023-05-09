1d3781769d42089749265c5be9eea4c3a0b258e2
AgentArachnid66@gmail.com <paulbrown.prb66@gmail.com>
2023-05-09 19:20:26 +0100

    Removed unnecessary Owning Object

--- Source/ClueSystem/Private/Libraries/ClueFastArray.cpp
+++ Source/ClueSystem/Private/Libraries/ClueFastArray.cpp
@@ -3,15 +3,15 @@
 
 void FReplicatedClueItem::PreReplicatedRemove(const FReplicatedAreaClueArray& InArraySerializer)
 {
-	UDebugFunctionLibrary::DebugLogWithObjectContext(InArraySerializer.OwningObject, "PreReplicatedRemove: " + AreaName);
+	UDebugFunctionLibrary::DebugLog( "PreReplicatedRemove: " + AreaName);
 }
 
 void FReplicatedClueItem::PostReplicatedAdd(const FReplicatedAreaClueArray& InArraySerializer)
 {
-	UDebugFunctionLibrary::DebugLogWithObjectContext(InArraySerializer.OwningObject, "PostReplicatedAdd: " + AreaName);
+	UDebugFunctionLibrary::DebugLog("PostReplicatedAdd: " + AreaName);
 }
 
 void FReplicatedClueItem::PostReplicatedChange(const FReplicatedAreaClueArray& InArraySerializer)
 {
-	UDebugFunctionLibrary::DebugLogWithObjectContext(InArraySerializer.OwningObject, "PostReplicatedChange: " + AreaName);
+	UDebugFunctionLibrary::DebugLog("PostReplicatedChange: " + AreaName);
 }
