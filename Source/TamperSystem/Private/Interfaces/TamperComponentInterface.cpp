227b3d63a33bec7890bfb79c09c411ed12db3024
Paul_AUDL <paulbrown.prb66@gmail.com>
2023-06-09 23:04:48 +0100

    Added Tamper Component Interface

---
+++ Source/TamperSystem/Private/Interfaces/TamperComponentInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/TamperComponentInterface.h"
+
+// Add default functionality here for any ITamperComponentInterface functions that are not pure virtual.
